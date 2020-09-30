34b2a4342b7c929efecc39764b00fb199fc35090
Lucide <Lucide@users.noreply.github.com>
2020-09-30 18:57:29 +0000

    added image save button added REXPaint charset export button 61612fd69768a88c466c6cbe31a065f9f342e7b1

--- sw.js
+++ sw.js
@@ -3000,7 +3000,7 @@
     //         self.skipWaiting();
     //     }
     // });
-    precacheAndRoute([{"revision":"a5e650c9530e59a0dbf8df227912af6c","url":"index.html"},{"revision":"3e2ab4028397367a6fef53bd73d52e65","url":"assets/AdobeBlank.otf.woff"},{"revision":"519a5da51bc46c3e98d4fa46a8bdeb53","url":"assets/DejaVuSansMono-webfont.woff"},{"revision":"b983f7106a1ac81d5da51fdbbc6ffed6","url":"assets/favicon.png"},{"revision":"9b462e75c40c6e10f43ed457fad03faa","url":"build/js/app.js"},{"revision":"68e6ab5de8801709c590f28a8425f7d9","url":"build/css/main.css"}]);
+    precacheAndRoute([{"revision":"382cef13287da515ec6371c14fe4d133","url":"index.html"},{"revision":"3e2ab4028397367a6fef53bd73d52e65","url":"assets/AdobeBlank.otf.woff"},{"revision":"519a5da51bc46c3e98d4fa46a8bdeb53","url":"assets/DejaVuSansMono-webfont.woff"},{"revision":"b983f7106a1ac81d5da51fdbbc6ffed6","url":"assets/favicon.png"},{"revision":"9e7e64c39328ffa221d18aa2f4f66e95","url":"build/js/app.js"},{"revision":"2e04161dae9dfbe3e2327dfe3a466038","url":"build/css/main.css"}]);
     cleanupOutdatedCaches();
     registerRoute(({ url }) => {
         return url.origin == "https://fonts.googleapis.com"
